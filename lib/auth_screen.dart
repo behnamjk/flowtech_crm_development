6da19609a2431441e3fdd42d2d3c78d5a1d522f4
behnamjk <behnam.webs@gmail.com>
2024-09-02 23:38:28 +0330

    integrate with appwrite sdk

---
+++ lib/auth_screen.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as models;
+
+class AuthScreen extends StatefulWidget {
+  final String title;
+
+  const AuthScreen({super.key, required this.title});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  models.User? loggedInUser;
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneNumberController = TextEditingController();
+  final TextEditingController countryController = TextEditingController();
+
+  bool isLoginPage = true;
+
+  void toggleForm() {
+    setState(() {
+      isLoginPage = !isLoginPage;
+    });
+  }
+
+  void login() {
+    String username = usernameController.text;
+    String password = passwordController.text;
+    // Implement login logic here
+    print('Logging in with username: $username, password: $password');
+  }
+
+  void signup() {
+    String username = usernameController.text;
+    String password = passwordController.text;
+    // Implement signup logic here
+    print('Signing up with username: $username, password: $password');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: usernameController,
+              decoration: InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Visibility(
+              visible: !isLoginPage,
+              child: Column(
+                children: [
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  TextField(
+                    controller: phoneNumberController,
+                    decoration: InputDecoration(
+                      labelText: 'Phone Number',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  TextField(
+                    controller: countryController,
+                    decoration: InputDecoration(
+                      labelText: 'Country',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: usernameController.text.isEmpty || passwordController.text.isEmpty
+                  ? null
+                  : isLoginPage ? login : signup,
+              child: Text(isLoginPage ? 'Login' : 'Sign Up'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextButton(
+              onPressed: toggleForm,
+              child: Text(isLoginPage
+                  ? 'Don\'t have an account? Sign Up'
+                  : 'Already have an account? Log In'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import './auth_screen.dart';
+import 'package:appwrite/appwrite.dart';
+
+
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Client client = Client();
+  client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('66d197ce0026df2f34da')
+    .setSelfSigned(status: true); // For self signed certificates, only use for development
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
@@ -14,128 +25,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flowtech CRM Software'),
+      home: AuthScreen(title: 'Flowtech CRM Software'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  const MyHomePage({super.key, required this.title});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController usernameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController phoneNumberController = TextEditingController();
-  final TextEditingController countryController = TextEditingController();
-
-  bool isLoginPage = true;
-
-  void toggleForm() {
-    setState(() {
-      isLoginPage = !isLoginPage;
-    });
-  }
-
-  void login() {
-    String username = usernameController.text;
-    String password = passwordController.text;
-    // Implement login logic here
-    print('Logging in with username: $username, password: $password');
-  }
-
-  void signup() {
-    String username = usernameController.text;
-    String password = passwordController.text;
-    // Implement signup logic here
-    print('Signing up with username: $username, password: $password');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            Visibility(
-              visible: !isLoginPage,
-              child: Column(
-                children: [
-                  TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextField(
-                    controller: phoneNumberController,
-                    decoration: InputDecoration(
-                      labelText: 'Phone Number',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextField(
-                    controller: countryController,
-                    decoration: InputDecoration(
-                      labelText: 'Country',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: usernameController.text.isEmpty || passwordController.text.isEmpty
-                  ? null
-                  : isLoginPage ? login : signup,
-              child: Text(isLoginPage ? 'Login' : 'Sign Up'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                foregroundColor: Colors.white,
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            TextButton(
-              onPressed: toggleForm,
-              child: Text(isLoginPage
-                  ? 'Don\'t have an account? Sign Up'
-                  : 'Already have an account? Log In'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
